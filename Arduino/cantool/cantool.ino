d929af73d409609e2d987510a44ebca4e1f37097
liuxueqin-ya <liuxueqin@csu.edu.cn>
2017-10-10 21:14:01 +0800

    arduino接收命令
    完成arduino接收字符串命令的工作以及简单的返回，但是没有想清楚\r究竟是以字符串形式存在还是以换行字符形式存在。另外，没有实现扩展帧和标准帧的输出，计划使用led灯的亮灭模拟该过程。（整个帧周期发送20次）

--- Arduino/cantool/cantool.ino
+++ Arduino/cantool/cantool.ino
@@ -1,13 +1,105 @@
-#include<Math.h>
+//#include<Math.h>
 
 boolean flag=false; //tool开启或关闭状态标志位
+int S_speed=10;
+String buff=""; //缓冲区
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("success");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    if(Serial.available() ){
+      buff=Serial.readString();
+      Serial.print(buff);
+  
+      String command=buff;
+      char c=command[0];
+      if(c=='O'){
+        //处理命令错误 
+        if(command.length()!=3){
+          fail();
+        }else{
+          open_s();
+        }
+      }else if(c=='V'){
+        //处理命令错误 
+        if(command.length()!=3){
+          fail();
+        }else{
+          Serial.println("SV2.5-HV2.0");
+        }
+      }else if(c=='C'){
+        //处理命令错误 
+        if(command.length()!=3){
+          fail();
+        }else{
+          close_s();
+        }
+      }else if(c=='S'){
+        //处理命令错误 
+        if(command.length()!=4){
+          fail();
+        }else{
+          Changespeed(command[1]);
+        }
+      }else if(c=='t'){
+        Sendstandardframe(command);
+      }else if(c=='T'){
+        Sendexternalframe(command);
+      }else{
+        fail();
+      }
+      buff="";//处理完该命令，清空缓存
+  }
 }
+
+void open_s(){
+  flag=true;
+  //返回成功
+  success();
+}
+
+void close_s(){
+  flag=false;
+  //返回成功
+  success();
+}
+
+void Changespeed(char NO_speed){
+//  Serial.println(NO_speed);
+   if(flag){
+    int num[]={10,20,50,100,125,250,500,800,1000};
+    if(NO_speed<'0' or NO_speed>'8'){//返回错误
+      fail();
+    }else{
+      int pos_speed=int(NO_speed-'0');
+      S_speed=num[pos_speed];
+      //返回成功
+      success();
+    }
+  }else{
+    fail();
+  }
+}
+/**
+ * 未完成
+ */
+void Sendstandardframe(String standardframe){
+  Serial.println(standardframe);
+}
+void Sendexternalframe(String externalframe){
+  Serial.println(externalframe);
+}
+
+void success(){
+  Serial.print("\r");
+}
+
+void fail(){
+  Serial.print(char(0x07));
+}
+
