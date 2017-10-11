abac22ce6f0883065e746364285b3a8d096323c3
WCY1996 <xiaoyuer0108@foxmail.com>
2017-10-11 21:26:26 +0800

    all

--- Arduino/cantool/cantool.ino
+++ Arduino/cantool/cantool.ino
@@ -1,8 +1,8 @@
 //#include<Math.h>
 
-boolean flag=false; //tool开启或关闭状态标志位
+boolean flag=false; //设置tool开启或关闭状态标志位
 int S_speed=10;
-String buff=""; //缓冲区
+String buff=""; //设置缓冲区
 
 void setup() {
   // put your setup code here, to run once:
@@ -53,19 +53,19 @@ void loop() {
       }else{
         fail();
       }
-      buff="";//处理完该命令，清空缓存
+      buff="";//处理完该命令，清空缓存区数据
   }
 }
 
 void open_s(){
   flag=true;
-  //返回成功
+  //返回成功信息
   success();
 }
 
 void close_s(){
   flag=false;
-  //返回成功
+  //返回成功信息
   success();
 }
 
@@ -73,12 +73,12 @@ void Changespeed(char NO_speed){
 //  Serial.println(NO_speed);
    if(flag){
     int num[]={10,20,50,100,125,250,500,800,1000};
-    if(NO_speed<'0' or NO_speed>'8'){//返回错误
+    if(NO_speed<'0' or NO_speed>'8'){//返回错误信息
       fail();
     }else{
       int pos_speed=int(NO_speed-'0');
       S_speed=num[pos_speed];
-      //返回成功
+      //返回成功信息
       success();
     }
   }else{
@@ -89,10 +89,10 @@ void Changespeed(char NO_speed){
  * 未完成
  */
 void Sendstandardframe(String standardframe){
-  Serial.println(standardframe);
+  Serial.println(standardframe);//打印标准帧
 }
 void Sendexternalframe(String externalframe){
-  Serial.println(externalframe);
+  Serial.println(externalframe);//打印额外帧
 }
 
 void success(){
