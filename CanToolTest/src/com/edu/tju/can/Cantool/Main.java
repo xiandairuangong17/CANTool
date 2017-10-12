d9d6a4ebc5efaf6bcdc3fec90d15777dfac7375e
1187325506@qq.com <1187325506@qq.com>
2017-10-12 21:44:22 +0800

    add mian

--- CanToolTest/src/com/edu/tju/can/Cantool/Main.java
+++ CanToolTest/src/com/edu/tju/can/Cantool/Main.java
@@ -2,7 +2,12 @@ package com.edu.tju.can.Cantool;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        SerialPort serialPort = SerialTool.openPort("COM12", 115200);
+        CANTool tool = new CANTool(serialPort);
+        SerialListener listener = new SerialListener(serialPort,tool);
+        tool.addListener(listener);
     }
+
 }
--- CanToolTest/src/com/edu/tju/can/Interface/SerialPortEventListener.java
+++
@@ -1,7 +0,0 @@
-package com.edu.tju.can.Interface;
-
-/**
- * Created by Admin on 2017/10/12.
- */
-public interface SerialPortEventListener {
-}
