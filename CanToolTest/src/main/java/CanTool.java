21eb3e9cae812ba6cc4d8db489742f3384467950
1187325506@qq.com <1187325506@qq.com>
2017-10-16 20:59:04 +0800

    移除冗余代码

--- CanToolTest/src/main/java/CanTool.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Admin on 2017/10/13.
- */
-public class CanTool {
-	private SerialPort serialPort;
-	private int state;
-	private int speed;
-}
--- CanToolTest/src/main/java/SerialListener.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Admin on 2017/10/13.
- */
-public class SerialListener {
-}
